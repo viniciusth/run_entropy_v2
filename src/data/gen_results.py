5b95aba67fad65a4e8ed6f7b222f77352b464cc4
Vinicius Hirschle <viniciustht@gmail.com>
2024-09-28 20:37:47 -0300

    implement multiprocessor

--- src/data/gen_results.py
+++ src/data/gen_results.py
@@ -1,13 +1,39 @@
 
 import os
 from typing import Tuple
+from multiprocessing import Queue, Process
 from src.data.gen_buckets import PROCESSORS
 from src.entropy.analysis import HYPERPERIOD_LEN, K, entropy
 from src.simso.model_builder import ACETModelBuilder
 from src.simso.sim_data import SimData
 
+# my cpu core count
+THREAD_COUNT = 8
 
 def gen_results(file_path: str):
+
+    def process_job(task_queue: Queue, response_queue: Queue):
+        while True:
+            task = task_queue.get()
+            if task is None:
+                break
+            fg_run, p_reorder = run_test(*task)
+            response_queue.put((fg_run, p_reorder))
+
+    def handle_result(fg_run, p_reorder, partial_result):
+        if fg_run is None:
+            partial_result["missed"] += 1
+            return
+
+        partial_result["data"]["fg_run"][p][i].append(fg_run)
+        partial_result["data"]["p_reorder"][p][i].append(p_reorder)
+
+    def save_partial_results(partial_result, name):
+        with open(f"{name}_partial.json", "w") as f:
+            f.write(str(partial_result))
+            print()
+            print("Saved partial results")
+
     input, partial_result = setup(file_path)
 
     line_sep = "\n" + "-"*50
@@ -15,34 +41,61 @@ def gen_results(file_path: str):
 
     current_idx = 0
     tests_ran_without_saving = 0
+    tasks_processing = 0
+    task_queue = Queue(20)
+    response_queue = Queue(20)
+
+    processes = []
+    for _ in range(THREAD_COUNT):
+        p = Process(target=process_job, args=(task_queue, response_queue))
+        p.start()
+        processes.append(p)
+
     for c, p in enumerate(PROCESSORS):
         print(line_sep)
         print("Processing", p)
         for i in range(10):
             print(line_sep)
-            print("Current percentage", i, "->", (i+1)*10)
+            print("Current percentage", i*10, "->", (i+1)*10)
             for test in input[c][i]:
-                if current_idx < partial_result["idx"]:
+                if current_idx <= partial_result["idx"]:
                     current_idx += 1
                     continue
                 print("Current idx", current_idx, end="\r")
+
+                if task_queue.full():
+                    if tests_ran_without_saving >= 32:
+                        partial_result["idx"] = current_idx
+                        tests_ran_without_saving = 0
+                        # wait for all tasks so current_idx is correct.
+                        while tasks_processing > 0:
+                            fg_run, p_reorder = response_queue.get()
+                            handle_result(fg_run, p_reorder, partial_result)
+                            tasks_processing -= 1
+                        save_partial_results(partial_result, name)
+                    else:
+                        fg_run, p_reorder = response_queue.get()
+                        handle_result(fg_run, p_reorder, partial_result)
+                        tasks_processing -= 1
+
                 current_idx += 1
-
-                fg_run, p_reorder = run_test(test, p)
-                if fg_run is None:
-                    partial_result["missed"] += 1
-                    continue
                 tests_ran_without_saving += 1
-                partial_result["data"]["fg_run"][p][i].append(fg_run)
-                partial_result["data"]["p_reorder"][p][i].append(p_reorder)
+                tasks_processing += 1
+                task_queue.put([test, p])
 
-                if tests_ran_without_saving == 5:
-                    partial_result["idx"] = current_idx
-                    with open(f"{name}_partial.json", "w") as f:
-                        f.write(str(partial_result))
-                    print()
-                    print("Saved partial results")
-                    tests_ran_without_saving = 0
+    while tasks_processing > 0:
+        fg_run, p_reorder = response_queue.get()
+        handle_result(fg_run, p_reorder, partial_result)
+        tasks_processing -= 1
+
+    print("Finished all tests, shutting down processes")
+    for _ in range(THREAD_COUNT):
+        task_queue.put(None)
+
+    for p in processes:
+        p.join()
+
+    print("All processes shut down, saving final results")
 
     with open(f"{name}_results.json", "w") as f:
         partial_result.pop("idx")
@@ -123,4 +176,11 @@ def run_scheduler(test, processors, scheduler):
         return None, "No hyperperiods found"
     scheduler_entropy = entropy(hp, len(test), processors)
     return scheduler_entropy, None # type: ignore
+
+def process_job(task_queue: Queue):
+    while True:
+        task = task_queue.get()
+        if task is None:
+            break
+        run_scheduler(*task)
 
